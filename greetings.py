23be8cfb35180bf48c36582058e9a3a0edd104f9
Albert-Aminev <aminev-albert123@yandex.ru>
2025-01-16 19:12:33 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Альберт'
+title = 'Домашнее задание'
+content = 'Выполнить до 17 числа'
+status = 'В процессе'
+created_date = '16-01-2025'
+issue_date = '17-01-2025'
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
